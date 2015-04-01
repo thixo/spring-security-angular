2d932f21eb2efee305a77b74106dd85f848781c0
Mark Sanders <masander@capgemini.com>
2015-04-01 15:59:00 +0100

    Mark - update login() function - rename authenticate() function as doAuthenticate() - - avoid confusion with authenticated flag - add logout() function - tab() function to mark home as active

--- ui/src/main/resources/static/js/hello.js
+++ ui/src/main/resources/static/js/hello.js
@@ -1,62 +1,79 @@
-angular.module('hello', [ 'ngRoute' ])
+angular.module('hello', [ 'ngRoute' ]
 
-    .config(function($routeProvider, $httpProvider) {
+    ).config(function($routeProvider, $httpProvider) {
 
         $routeProvider.when('/', {
             templateUrl : 'home.html',
-            controller  : 'home'
+            controller : 'home'
         }).when('/login', {
             templateUrl : 'login.html',
-            controller  : 'navigation'
+            controller : 'navigation'
         }).otherwise('/');
 
         $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-    })
+    }).controller('navigation', function($rootScope, $scope, $http, $location, $route) {
+        
+        $scope.tab = function(route) {
+            return $route.current && route === $route.current.controller 
+        }
+        
+        var doAuthenticate = function(credentials, callback) {
 
-    .controller('home', function($scope, $http) {
-        $http.post('/resource/').success(function(data) {
-            $scope.greeting = data
-        })
-    })
-    
-    .controller('navigation', function($rootScope, $scope, $http, $location) {
-        var authenticate = function(credentials, callback) {
-            
             var headers = {};
-            
+
             if (credentials) {
-                var value = "Basic " + btoa(credentials.username + ':' + credentials.password );
+                var value = "Basic " + btoa(credentials.username + ":" + credentials.password);
                 headers = { authorization : value };
             }
-            
-            $http.get('user', {headers : headers})
-            .success(function(data) {
-                if (data.name) {
-                    $rootScope.authenticated = true;
-                } else {
+
+            $http.get('user', { headers : headers }
+                ).success(function(data) {
+                    if (data.name) {
+                        $rootScope.authenticated = true;
+                    } else {
+                        $rootScope.authenticated = false;
+                    }
+                    callback && callback($rootScope.authenticated);
+                }).error(function() {
                     $rootScope.authenticated = false;
-                }
-                callback && callback();
-            })
-            .error(function() {
-                $rootScope.authenticated = false;
-                callback && callback();
-            })
-        };
-        
-        authenticate();
-        
+                    callback && callback(false);
+                });
+        }
+
+        doAuthenticate();
+
         $scope.credentials = {};
+
         $scope.login = function() {
-            authenticate($scope.credentials, function() {
-                if ($rootScope.authenticated) {
-                    $location.path("/");
-                    $scope.error = false;
-                } else {
-                    $location.path("/login");
-                    $scope.error = true;
-                }
-            });
-        };
+            doAuthenticate(
+                $scope.credentials,
+                function(authenticated) {
+                    if (authenticated) {
+                        console.log("Login succeeded");
+                        $location.path("/");
+                        $scope.error = false;
+                        $rootScope.authenticated = true;
+                    } else {
+                        console.log("Login failed");
+                        $location.path("/login");
+                        $scope.error = true;
+                        $rootScope.authenticated = false;
+                    }
+                });
+        }
+
+        $scope.logout = function() {
+            $http.post('logout', {}
+                ).success(function() {
+                    $rootScope.authenticated = false;
+                    $location.path('/');
+                }).error(function(data) {
+                    $rootScope.authenticated = false;
+                });
+        }
+
+    }).controller( 'home',  function($scope, $http) {
+        $http.get('/resource/'
+            ).success(function(data) { $scope.greeting = data; } );
     });
